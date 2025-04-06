b0e3d3ffd9400d139ac8b564b03f713e18d29039
AF <gulabsinghrajput008@gmail.com>
2025-04-06 21:04:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ class Config:
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://besiv18855:ES5IePZRHrVxRvtQ@cluster0.peo9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    DATABASE_NAME = environ.get("DATABASE_NAME", "rrrr")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7170452349').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002030723564'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002030723564") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
